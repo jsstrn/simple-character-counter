06cea59639da78fa86f28fd860c4d4903227b0e2
jsstrn <jessternrays@gmail.com>
2015-09-14 01:53:53 +0800

    Add word count functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,26 @@ $(document).ready(function(){
     // button clicked
     $('button').click(function(){
         var text = $('textarea').val(); // get text from textarea
-        var charCount = charCounter(text); // count number of words in text
-        $("#results").text(charCount);
-    });
-    function charCounter(text) {
         var charCount = 0;
-        for (var i = 0; i < text.length; i++) {
-            charCount++;
-        };
-        return charCount;
-    }
+        if (text) {
+            charCount = text.length;
+        }
+        var wordList = text.match(/\w+/g);
+        wordCount = 0;
+        if (wordList) {
+            wordCount = wordList.length;
+        }
+        $("#char").text(charCount);
+        $("#words").text(wordCount);
+        var results = "There are " + charCount + " characters and "+ wordCount +" words.";
+        $('#results').text(results);
+        var displayText = "";
+        if (wordCount === 0) {
+            
+        } else if (wordCount > 50) {
+
+        } else {
+
+        }
+    });
 });
